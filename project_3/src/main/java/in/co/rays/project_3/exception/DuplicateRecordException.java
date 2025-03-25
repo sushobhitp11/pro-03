6803c735cd47cf9d82492b29617a2451d41ba092
sushobhitp11 <sushobhitp11@gmail.com  git config --global user.name Your>
2025-03-25 17:15:07 +0530

    done:

---
+++ project_3/src/main/java/in/co/rays/project_3/exception/DuplicateRecordException.java
@@ -0,0 +1,14 @@
+package in.co.rays.project_3.exception;
+
+public class DuplicateRecordException extends Exception {
+	   /**
+	    * 
+	    * @author Sushobhit pandey
+	    */
+	private static final long serialVersionUID = 1L;
+
+	public DuplicateRecordException(String msg){
+		super(msg);
+	}
+	
+}
